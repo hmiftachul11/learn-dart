1ca9d0ef1c66a01b0dd5e70773c407f43226480f
hmiftachul11 <hmiftachul11@gmail.com>
2024-01-04 11:20:50 +0700

    Set Dart

---
+++ Programming Zaman Now/set.dart
@@ -0,0 +1,18 @@
+void main() {
+  Set<int> numbers = {};
+  final numberDouble = <double>{};
+
+  print(numbers);
+
+  var names = <String>{};
+  names.add('Miftachul');
+  names.add('Huda');
+  names.add('Huda');
+
+  print(names);
+  print(names.length);
+
+  /// Deklarasi secara langsung
+  var sekolah = <String>{'SD', 'SMP', 'SMA', 'Perguruan Tinnggi'};
+  print(sekolah);
+}
