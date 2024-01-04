8ed27a9fc036fd0e8b5b10d38ebf1467321b49f6
hmiftachul11 <hmiftachul11@gmail.com>
2024-01-04 21:04:13 +0700

    Ternary Dart

---
+++ Programming Zaman Now/ternary_operator.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Contoh bukanternary
+  var nilai = 75;
+  // String ucapan;
+
+  // if (nilai >= 75) {
+  //   ucapan = 'Selamat Anda Lulus';
+  // } else {
+  //   ucapan = 'Silahkan Coba lagi';
+  // }
+
+  // print(ucapan);
+
+  String ucapan = nilai >= 75 ? 'Lulus' : 'Coba Lagi';
+
+  print(ucapan);
+}
