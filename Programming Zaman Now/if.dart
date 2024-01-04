9017f7380777b7f590a536349354ce1ab99e54ad
hmiftachul11 <hmiftachul11@gmail.com>
2024-01-04 12:08:17 +0700

    If dan Switch

---
+++ Programming Zaman Now/if.dart
@@ -0,0 +1,9 @@
+void main() {
+  var nilai = 50;
+  var absen = 90;
+  if (nilai >= 75 && absen >= 75) {
+    print('Anda Lulus');
+  } else {
+    print('Anda tidak lulus');
+  }
+}
---
+++ Programming Zaman Now/switch_case.dart
@@ -0,0 +1,17 @@
+void main() {
+  var nilai = 'A';
+  switch (nilai) {
+    case 'A':
+      print('Wow Anda Lulus Dengan Baik');
+      break;
+    case 'B':
+    case 'C':
+      print('Anda Lulus');
+      break;
+    case 'D':
+      print('Anda Lidak Lulus');
+      break;
+    default:
+      print('Mungkia Anda Salah Jucusan');
+  }
+}
